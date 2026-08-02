fd9c7687bda09629055110d9ce3c1b1f35bca17e
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-08-03 04:46:57 +0530

    add CORS support

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,9 +1,27 @@
 import express, { Application, Response } from "express";
+import cors from "cors";
+import { env } from "@/config";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
 
+// ==================== Log ===============================
+if (env.isDev) {
+  app.use((req, res, next) => {
+    console.table({
+      req: {
+        method: req.method,
+        url: `${req.protocol}://${req.hostname}:${env.PORT}${req.url}`,
+        statusCode: res.statusCode,
+      },
+    });
+    next();
+  });
+}
+
+// ================== Root Route =========================
 app.get("/", (_, res: Response) =>
   res.json({ message: "Wellcome to Procura AI APIs" })
 );
